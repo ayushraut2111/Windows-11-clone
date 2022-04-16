ede70677797896e2ce439c57eaff8faf9b9dab2a
Ayush Chaurasia <94608049+ayushraut2111@users.noreply.github.com>
2022-04-16 21:28:19 +0530

    Add files via upload

---
+++ windows11.js
@@ -0,0 +1,12 @@
+let a=document.getElementById("taskimage1");
+        a.addEventListener("click",()=>{
+            let b=document.getElementById("start1");
+            if(b.style.bottom=="-673px")
+            {
+                b.style.bottom="55px"
+            }
+            else
+            {
+                b.style.bottom="-673px"
+            }
+        });
